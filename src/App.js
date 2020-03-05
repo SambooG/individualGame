64356af4106486e532e7f49a84ab8cc50f7b73cb
Samantha Gaiser <sgaiser3@gmail.com>
2020-03-05 16:08:12 -0500

    adding a slash

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Heading from './heading'
 import Gamepage from './gamepage';
 import Footer from './footer';
-import Directions from '.directions'
+import Directions from './directions'
 
 function App() {
   return (
