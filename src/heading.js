22723d4287afa74a1d0cdae89df88e968b0ea76c
Samantha Gaiser <sgaiser3@gmail.com>
2020-03-05 16:21:09 -0500

    removing one last thing

--- src/heading.js
+++ src/heading.js
@@ -4,7 +4,6 @@ import "./index.css";
 function heading() {
     return <div class="heading">
                 <h1>Click Pics!</h1>
-                    <h2>Click each photo only once or lose!</h2>
             </div>
             
 }
