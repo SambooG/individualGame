2d9000953d35341caaeb961a9b6407efb1070b55
Samantha Gaiser <sgaiser3@gmail.com>
2020-03-05 11:38:04 -0500

    Updates

---
+++ precache-manifest.516eaebd293b1eb3d6821dab5d9b3ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46510764bf6743095b463384ed298498",
+    "url": "/individualGame/index.html"
+  },
+  {
+    "revision": "af47ebcc25b550d8a1cd",
+    "url": "/individualGame/static/css/main.54bc4146.chunk.css"
+  },
+  {
+    "revision": "aeb991bd404ee59b33a5",
+    "url": "/individualGame/static/js/2.d99f762c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/individualGame/static/js/2.d99f762c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af47ebcc25b550d8a1cd",
+    "url": "/individualGame/static/js/main.c08f29bc.chunk.js"
+  },
+  {
+    "revision": "92f8ebe0cdf45b63b34e",
+    "url": "/individualGame/static/js/runtime-main.9eec138a.js"
+  }
+]);
