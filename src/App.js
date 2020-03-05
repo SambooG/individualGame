f38b7eb72b0ee53cbbfa142ea93cf5b1e382996b
Samantha Gaiser <sgaiser3@gmail.com>
2020-03-05 16:06:33 -0500

    one more fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Heading from './heading'
 import Gamepage from './gamepage';
 import Footer from './footer';
-import Directions from '.Directions'
+import Directions from '.directions'
 
 function App() {
   return (
--- src/Directions.js
+++ src/Directions.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./index.css";
 
-function Directions() {
+function directions() {
     return <div class="directions">
                     <h2>Click each photo only once or lose!</h2>
             </div>
             
 }
 
-export default Directions;
+export default directions;
